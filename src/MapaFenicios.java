c5139e9f69f449fafec381abb3aaf11bf3f4f5bb
Filipe Giroleti <filipe.fuentes@edu.pucrs.br>
2023-06-04 13:42:21 -0300

    minor changes

--- src/MapaFenicios.java
+++ src/MapaFenicios.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class MapaFenicios {
     /**
-     * Lê um arquivo de texto com os dados do mapa salva-os em uma matriz de chars
+     * Lê um arquivo de texto com os dados do mapa e salva-os em uma matriz de chars
      * @param caminhoArq caminho do arquivo de texto
      * @return matriz de chars com os dados do arquivo
      */
@@ -43,25 +43,32 @@ public class MapaFenicios {
     }
 
     /**
-     * Salva todas as posicoes dos portos em um dicionario, mantdestinoo a ordem numerica deles
+     * Salva todas as posicoes dos portos em um dicionario, mantendo a ordem numerica deles
      * @param matriz matriz de chars com os dados do mapa
      * @return dicionario com as posicoes dos portos
      */
-    private static TreeMap<Integer, Tuple> getPositions(char[][] matriz) {
-        TreeMap<Integer, Tuple> numberPositions = new TreeMap<>();
+    private static TreeMap<Integer, Tuple> getPosicoes(char[][] matriz) {
+        TreeMap<Integer, Tuple> posNumeros = new TreeMap<>();
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                char element = matriz[i][j];
-                if (Character.isDigit(element)) {
-                    int digit = Character.getNumericValue(element);
-                    numberPositions.put(digit, new Tuple(i, j));
+                char elemento = matriz[i][j];
+                if (Character.isDigit(elemento)) {
+                    int digito = Character.getNumericValue(elemento);
+                    posNumeros.put(digito, new Tuple(i, j));
                 }
             }
         }
-        return numberPositions;
+        return posNumeros;
     }
 
+    /**
+     * Acha o menor caminho entre dois pontos na matriz de entrada
+     * @param matriz matriz de chars com os dados do mapa
+     * @param origem posicao de origem
+     * @param destino posicao de destino
+     * @return distancia entre os dois pontos
+     */
     public static int achaMenorCaminho(char[][] matriz, Tuple origem, Tuple destino) {
         int linhas = matriz.length;
         int colunas = matriz[0].length;
@@ -76,7 +83,8 @@ public class MapaFenicios {
             }
         }
 
-        // Definir as direções (cima, baixo, esquerda, direita)
+        // Vetores para explorar as posições vizinhas
+        // (cima, baixo, esquerda, direita)
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
 
@@ -126,8 +134,8 @@ public class MapaFenicios {
         String caminhoArq = "mapa0.txt";
         char[][] matriz = lerArquivoMapa(caminhoArq);
 
-        // salvar todas as posicoes dos portos em uma lista
-        TreeMap<Integer, Tuple> posicaoPortos = getPositions(matriz);
+        // salvar todas as posicoes dos portos em um dicionario para manter a ordem numerica deles
+        TreeMap<Integer, Tuple> posicaoPortos = getPosicoes(matriz);
 
         int caminhoTotal = 0;
         int i = 1;
@@ -155,7 +163,6 @@ public class MapaFenicios {
             }
         }
         System.out.println(caminhoTotal);
-
     }
 }
 
